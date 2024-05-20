33ef6af1bba3c412f595566a2378a8937fb559b5
Vlada <ais191telegina@yandex.ru>
2024-05-20 16:10:16 +0500

    the second lesson changes https://www.youtube.com/watch?v=150R89VmJgc&list=PLV_4DSIw2vvI3_a6L_z5AlNaIdFNqQlW2&index=4

---
+++ dispatcher/src/main/java/ru/xenozar/controller/UpdateController.java
@@ -0,0 +1,93 @@
+package ru.xenozar.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.xenozar.service.UpdateProducer;
+import ru.xenozar.utils.MessageUtils;
+
+import static ru.xenozar.model.RabbitQueue.*;
+
+@Component
+@Log4j
+public class UpdateController {
+    private TelegramBot telegramBot;
+    private final MessageUtils messageUtils;
+    private UpdateProducer updateProducer;
+
+    public UpdateController (MessageUtils messageUtils, UpdateProducer updateProducer) {
+        this.messageUtils = messageUtils;
+        this.updateProducer=updateProducer;
+    }
+    public void registerBot(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    public void processUpdate(Update update) {
+        if (update == null) {
+            log.error("Received update is null");
+            return;
+        }
+
+        if (update.getMessage() != null) {
+            distributeMessagesByType(update);
+        } else {
+            log.error("Unsupported message type is received: " + update);
+        }
+    }
+
+    private void distributeMessagesByType(Update update) {
+        var message = update.getMessage();
+        if (message.getText() != null) {
+            processTextMessage(update);
+        } else if (message.getDocument() != null) {
+            processDocMessage(update);
+        } else if (message.getPhoto() != null) {
+            processPhotoMessage(update);
+        } else {
+            setUnsupportedMessageTypeView(update);
+        }
+    }
+
+    private void setUnsupportedMessageTypeView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update,
+                "Неподдерживаемый тип сообщения.");
+        setView(sendMessage);
+    }
+
+    private void setFileIsReceivedView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update,
+                "Файл получен, обрабатывается...");
+        setView(sendMessage);
+    }
+
+    private void setView(SendMessage sendMessage) {
+        telegramBot.sendAnswerMessage(sendMessage);
+    }
+
+    private void processPhotoMessage(Update update) {
+        updateProducer.produce(PHOTO_MESSAGE_UPDATE,
+                update);
+        setFileIsReceivedView(update);
+    }
+
+    private void processDocMessage(Update update) {
+        updateProducer.produce(DOC_MESSAGE_UPDATE,
+                update);
+        setFileIsReceivedView(update);
+    }
+
+    private void processTextMessage(Update update) {
+        updateProducer.produce(TEXT_MESSAGE_UPDATE,
+                update);
+    }
+
+    public UpdateProducer getUpdateProducer() {
+        return updateProducer;
+    }
+
+    public void setUpdateProducer(UpdateProducer updateProducer) {
+        this.updateProducer = updateProducer;
+    }
+}
