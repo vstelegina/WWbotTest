518b6d481bc9237cd0461599388d960b004d38d2
Vlada <ais191telegina@yandex.ru>
2024-05-29 23:29:50 +0500

    work with node

---
+++ node/src/main/java/ru/xenozar/service/impl/ProducerServiceImpl.java
@@ -0,0 +1,23 @@
+package ru.xenozar.service.impl;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.xenozar.service.ProducerService;
+
+import static ru.xenozar.model.RabbitQueue.ANSWER_MESSAGE;
+
+@Service
+public class ProducerServiceImpl implements ProducerService {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public ProducerServiceImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void produceAnswer(SendMessage sendMessage) {
+        rabbitTemplate.convertAndSend(ANSWER_MESSAGE, sendMessage);
+    }
+}
